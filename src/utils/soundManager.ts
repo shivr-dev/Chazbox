aa4161cfe1a9b3913ddb70e6ece0cf659dc98237
shivr-dev <shivrdream@gmail.com>
2026-02-28 11:27:01 +0800

    Create soundManager.ts

---
+++ src/utils/soundManager.ts
@@ -0,0 +1,43 @@
+class SoundManager {
+  private static instance: SoundManager;
+  private sounds: Record<string, HTMLAudioElement> = {};
+
+  private constructor() {
+    this.preloadSounds();
+  }
+
+  public static getInstance(): SoundManager {
+    if (!SoundManager.instance) {
+      SoundManager.instance = new SoundManager();
+    }
+    return SoundManager.instance;
+  }
+
+  private preloadSounds() {
+    const soundFiles = [
+      'sounds_click',
+      'sounds_button',
+      'toast',
+      'pop',
+      'sounds_drawer_open',
+      'sounds_drawer_close'
+    ];
+
+    soundFiles.forEach(name => {
+      const audio = new Audio(`/sounds/${name}.ogg`);
+      audio.preload = 'auto';
+      this.sounds[name] = audio;
+    });
+  }
+
+  public play(name: string) {
+    const sound = this.sounds[name];
+    if (sound) {
+      // Clone the node to allow overlapping sounds
+      const clone = sound.cloneNode() as HTMLAudioElement;
+      clone.play().catch(e => console.warn('Audio play failed:', e));
+    }
+  }
+}
+
+export const soundManager = SoundManager.getInstance();
